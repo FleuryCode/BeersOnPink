6da323ef5e6d16d576ac70f27f19c77972542816
Julien Fleury <julien@fleuryphotography.com>
2022-04-10 17:51:57 +0200

    Added onChange functions on state values for contact form.

--- src/components/contactForm/contactForm.component.jsx
+++ src/components/contactForm/contactForm.component.jsx
@@ -1,55 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomButton from "../customButton/customButton.component";
 import CustomInput from "../customInput/customInput.component";
 import CustomTextField from "../customTextField/customTextField.component";
 import './contactForm.styles.scss';
 
-class ContactForm extends React.Component {
-    constructor() {
-        super();
+const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
 
-        this.state ={
-            name: '',
-            email: '',
-            message: '',
+    // On Input Change
+    const onInputChangeHandle = (event) => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'message':
+                setMessage(value);
+                break;
+            default:
+                break;
         }
     }
-    render() {
-        return (
-            <div className="contactFormContainer container-fluid">
-                <div className="row">
-                    <div className="col-12 d-flex justify-content-center">
-                        <h2>WE WOULD LOVE TO HEAR FROM YOU</h2>
-                    </div>
-                    <form>
-                        <div className="row">
-                            <div className="col-12">
-                                <label htmlFor="name">Full Name</label>
-                            </div>
-                            <div className="col-12">
-                                <CustomInput id={'name'} type={'text'} name={'name'} placeholder={'John Smith'} />
-                            </div>
-                            <div className="col-12">
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div className="col-12">
-                                <CustomInput id={'email'} type={'email'} name={'email'} placeholder={'example@email.com'} />
-                            </div>
-                            <div className="col-12">
-                                <label htmlFor="message">Message</label>
-                            </div>
-                            <div className="col-12">
-                                <CustomTextField id={'message'} name={'message'} placeholder={'Message'} />
-                            </div>
-                            <div className="col-2 ms-auto me-auto">
-                                <CustomButton staticMessage={'Send Message'} />
-                            </div>
-                        </div>
-                    </form>
+    return (
+        <div className="contactFormContainer container-fluid">
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center">
+                    <h2>WE WOULD LOVE TO HEAR FROM YOU</h2>
                 </div>
+                <form>
+                    <div className="row">
+                        <div className="col-12">
+                            <label htmlFor="name">Full Name</label>
+                        </div>
+                        <div className="col-12">
+                            <CustomInput id={'name'} type={'text'} name={'name'} placeholder={'John Smith'} value={name} onChange={onInputChangeHandle} />
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="email">Email</label>
+                        </div>
+                        <div className="col-12">
+                            <CustomInput id={'email'} type={'email'} name={'email'} placeholder={'example@email.com'} value={email} onChange={onInputChangeHandle} />
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="message">Message</label>
+                        </div>
+                        <div className="col-12">
+                            <CustomTextField id={'message'} name={'message'} placeholder={'Message'} value={message} onChange={onInputChangeHandle} />
+                        </div>
+                        <div className="col-2 ms-auto me-auto">
+                            <CustomButton staticMessage={'Send Message'} />
+                        </div>
+                    </div>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default ContactForm;
--- src/components/customInput/customInput.component.jsx
+++ src/components/customInput/customInput.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './customInput.styles.scss';
 
-const CustomInput = ({ id, type, name, placeholder, onChange }) => {
+const CustomInput = ({ id, type, name, placeholder, value, onChange }) => {
     return (
         <div className="customInputContainer">
-            <input required className="inputField" id={id} type={type} name={name} placeholder={placeholder} />
+            <input required className="inputField" id={id} type={type} name={name} placeholder={placeholder} onChange={onChange} value={value} />
         </div>
     );
 }
