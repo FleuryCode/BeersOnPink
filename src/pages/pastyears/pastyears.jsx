cccef883389adab967855233bbcebc06564befcc
Julien Fleury <julien@fleuryphotography.com>
2022-04-15 17:47:59 +0200

    Added pastYears page. Now time to add specifics and alter data based on the actual year.

--- src/pages/pastyears/pastyears.jsx
+++ src/pages/pastyears/pastyears.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import './pastyears.styles.scss';
 
-class PastYears extends React.Component {
-    render() {
-        return(
-            <div className="pastYearsContainer">
-                <h1>Past Years</h1>
+const PastYears = () => {
+    return (
+        <div className="pastYearsContainer container-fluid" >
+            <div className="row d-flex justify-content-center">
+                <div className="col-12 col-md-5">
+                    <div className="container2019">
+                        <h2>2019</h2>
+                    </div>
+                </div>
+                <div className="col-12 col-md-5">
+                    <div className="container2018">
+                        <h2>2018</h2>
+                    </div>
+                </div>
+                <div className="col-12 col-md-5">
+                    <div className="container2017">
+                        <h2>2017</h2>
+                    </div>
+                </div>
+                <div className="col-12 col-md-5">
+                    <div className="container2016">
+                        <h2>2016</h2>
+                    </div>
+                </div>
             </div>
-        );
-    }
+        </div >
+    );
+
 }
 
 export default PastYears;
---
+++ src/pages/specificPastYear/specificPastYear.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './specificPastYear.styles.scss';
+
+const SpecificPastYearPage = () => {
+    return(
+        <div className="specificPastYearPageContainer">
+
+        </div>
+    );
+}
+
+export default SpecificPastYearPage;
